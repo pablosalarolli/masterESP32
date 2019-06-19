c57096934d104c80edf40cb1b61f2361603b3c6b
Pablo Salarolli <pablo_salarolli@hotmail.com>
2019-06-19 18:22:01 -0300

    Update master_main_WiFi.ino
    WiFi + serial juntos

--- src/master_main_WiFi/master_main_WiFi.ino
+++ src/master_main_WiFi/master_main_WiFi.ino
@@ -70,18 +70,18 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("flag 0");
+  //  Serial.println("flag 0");
   WiFiClient client = server.available();
   if (client) {
     while (client.connected()) {
-      Serial.println("flag 1");
+      //      Serial.println("flag 1");
       switch (estado) {
         case AGUARDANDO:
-          Serial.println("Aguardando");
+          //          Serial.println("Aguardando");
           tAgora = millis();
           if (client.available()) {
             estado = RECEBE_MSG_PC;
-            Serial.println("flag 2");
+            //            Serial.println("flag 2");
           }
           else if ((tAgora - tAnt) >= dt) {
             tAnt = tAgora;
@@ -96,7 +96,7 @@ void loop() {
           break;
 
         case RECEBE_MSG_PC:
-          Serial.println("flag 3");
+          //          Serial.println("flag 3");
           i = 0;
           while (client.available() > 0) { //Enquanto houver transmissão de dados:
             if (i < 4) {                    //se nº bytes recebidos<4:
@@ -238,6 +238,155 @@ void loop() {
       delay(5);
     }
     client.stop();
+  }
+  else {
+    switch (estado) {
+      case AGUARDANDO:
+        //            Serial.println("Aguardando");
+        tAgora = millis();
+        if (Serial.available()) {
+          estado = RECEBE_MSG_PC;
+          //        Serial.println("RECEBI PC");
+        }
+        else if ((tAgora - tAnt) >= dt) {
+          //        Serial.println("Timeout aquisicao");
+          tAnt = tAgora;
+          opcode = 0b0110; //verifica se SP = VP ATUALIZAR OPCODE!!!
+          addr = SLAVE_1;
+          ambos = 1;
+          estado = ENVIA_MSG_SLAVE;
+          proximo_estado = ATUALIZA_LEDS;
+        }
+        else if (aquisicao_continua_flag)
+          estado = AQUISICAO_CONTINUA;
+        break;
+
+      case RECEBE_MSG_PC:
+        flag = recebeMensagem(SERIAL_PORT_PC, &addr, &opcode, &dado);
+        if (!flag) {
+          proximo_estado = RESPONDE_PC;
+          trataMSGPC();
+        }
+        else
+          estado = REPORTA_ERRO;
+        break;
+
+      case RESPONDE_PC:
+        //      Serial.println("Estado: RESPONDE_PC");
+        enviaMensagem(SERIAL_PORT_PC, addr, opcode, dado);
+        if (ambos) {
+          ambos = 0;
+          addr = SLAVE_2;
+          estado = ENVIA_MSG_SLAVE;
+        }
+        else
+          estado = AGUARDANDO;
+        break;
+
+      case ENVIA_MSG_SLAVE:
+        //      Serial.println("Estado: ENVIA_MSG_SLAVE");
+        //      Serial.println("Estado: AGUARDA_RESPOSTA_SLAVE");
+        //      Serial.print("addr ");
+        //      Serial.println(addr, HEX);
+        //      Serial.print("opcode ");
+        //      Serial.println(opcode, HEX);
+        //      Serial.print("DADO ");
+        //      Serial.println(dado, HEX);
+        //      Serial.print("ambos ");
+        //      Serial.println(ambos, HEX);
+        enviaMensagem(SERIAL_PORT_BUS, addr, opcode, dado);
+        estado = AGUARDA_RESPOSTA_SLAVE;
+        tAgoraTimeOut = millis();
+        tAntTimeOut = tAgoraTimeOut;
+        break;
+
+      case AGUARDA_RESPOSTA_SLAVE:
+
+        if (Serial2.available()) {
+          estado = TRATA_RESPOSTA_SLAVE;
+        }
+        else {
+          tAgoraTimeOut = millis();
+          //        Serial.println(tAgoraTimeOut - tAntTimeOut, DEC);
+          if ((tAgoraTimeOut - tAntTimeOut) >= timeout) {
+            tAntTimeOut = tAgoraTimeOut;
+            flag = 5; // flag de timeout
+            estado = RETRANSMISSAO;
+            //          Serial.println("Deu pau");
+          }
+        }
+        break;
+
+      case TRATA_RESPOSTA_SLAVE:
+        //      Serial.println("Estado: TRATA_RESPOSTA_SLAVE");
+        addrBackup = addr;
+        opcodeBackup = opcode;
+        dadoBackup = dado;
+        flag = recebeMensagem(SERIAL_PORT_BUS, &addr, &opcode, &dado);
+        if (!flag) {
+          estado = proximo_estado;
+          if (addr == MASTER_ADDR) {
+            addr = addrBackup;
+          }
+        }
+        else {
+          estado = RETRANSMISSAO;
+        }
+        break;
+
+      case ATUALIZA_LEDS:
+        //      Serial.println("Estado: ATUALIZA_LEDS");
+        //      Serial.println("addr");
+        //      Serial.print(addr, HEX);
+        //      Serial.println("DADO");
+        //      Serial.print(dado, HEX);
+        //      Serial.println("ambos");
+        //      Serial.print(ambos, HEX);
+        atualizaLEDSP(addr, dado); // Atualiza o Led do escravo em addr com o valor de dado
+        if (ambos) {
+          //        Serial.println("ambos: ambos");
+          ambos = 0;
+          addr = SLAVE_2;
+          estado = ENVIA_MSG_SLAVE;
+        }
+        else
+          estado = AGUARDANDO;
+        break;
+
+      case AQUISICAO_CONTINUA:
+        //      Serial.println("Estado: AQUISICAO_CONTINUA");
+        estado = ENVIA_MSG_SLAVE;
+        break;
+
+      case RETRANSMISSAO:
+        //      Serial.println("Estado: RETRANSMISSAO");
+        if (contRetransmissao >= 3) {
+          flag = 6;
+          estado = REPORTA_ERRO;
+          contRetransmissao = 0;
+        }
+        else {
+          addr = addrBackup;
+          opcode = opcodeBackup;
+          dado = dadoBackup;
+          estado = ENVIA_MSG_SLAVE;
+          contRetransmissao++;
+        }
+        break;
+
+      case REPORTA_ERRO:
+        //      Serial.println("Estado: REPORTA_ERRO");
+        estado = AGUARDANDO;
+        break;
+
+      default:
+        //      Serial.println("Estado: DEFAULT");
+        estado = AGUARDANDO;
+        break;
+    }
+
+    //  enviaMensagem(0x02, 0x01, 0x02F5);
+    delay(5);
   }
 }
 
